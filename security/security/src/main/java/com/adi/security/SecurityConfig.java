7411f445a3bc705da721d398e702c202c64a4f71
[Aditya <[www.aditya117@gmail.com]>
2025-01-28 09:31:37 +0530

    hashing and h2 database security

--- security/security/src/main/java/com/adi/security/SecurityConfig.java
+++ security/security/src/main/java/com/adi/security/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.adi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.http.UserDetailsServiceFactoryBean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,22 +27,44 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated());
         // http.formLogin(withDefaults());
         http.sessionManagement(session ->
                 session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)));  // cookies will not get generated
         http.httpBasic(withDefaults());
+
+        http.headers(headers->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user1 = User.withUsername("user1").password("{noop}adi").roles("USER").build();
-        // {noop} stores in encoded
-        UserDetails admin = User.withUsername("admin").password("{noop}adi").roles("ADMIN").build();
+//        UserDetails user1 = User.withUsername("user1").password("{noop}adi").roles("USER").build();
+//        // {noop} stores in encoded
+//        UserDetails admin = User.withUsername("admin").password("{noop}adi").roles("ADMIN").build();
+        // return new InMemoryUserDetailsManager(user1,admin);
 
-        return new InMemoryUserDetailsManager(user1,admin);
+        UserDetails user = User.withUsername("user").password(passwordEncoder().encode("user")).roles("USER").build();
+        // {noop} stores in encoded
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
